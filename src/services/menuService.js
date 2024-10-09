86dd5bd212f6a2028a11fd4b7b8925f0338dea0e
goubexlv <ngoubenejunior@gmail.com>
2024-10-09 23:57:30 +0200

    Affichage du menu lie

--- src/services/menuService.js
+++ src/services/menuService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = "http://127.0.0.1:8080/api";
+/* const url = "http://127.0.0.1:8080/api"; */
+const url = "https://restaurantbackend-vrh4.onrender.com/api";
 class Menus{
 
     getMenu(){
