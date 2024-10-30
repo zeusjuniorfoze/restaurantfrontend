e754be22d8f7967c527ed74aabb534bb8c2c1709
goubexlv <ngoubenejunior@gmail.com>
2024-10-30 16:09:23 +0100

    reservation

--- src/services/menuService.js
+++ src/services/menuService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = "http://127.0.0.1:8080/api"; 
-/* const url = "https://restaurantbackend-flax.vercel.app/api"; */
+//const url = "http://127.0.0.1:8080/api"; 
+const url = "https://restaurantbackend-flax.vercel.app/api"; 
 class Menus{
 
     getMenu(){
